89378bc8c152c9c4edc48ecc29a5347f14698b44
EC2 Default User <maherahemant1@gmail.com>
2020-05-13 04:37:10 +0000

    use of %s, %d

---
+++ use_of_%s_%d/%s_%d.py
@@ -0,0 +1,11 @@
+#!/home/ec2-user/my_app/env/bin/python
+''' %s - string, %d - integar '''
+name = "John"
+age = 23
+print("%s is %d years old." % (name, age))
+''' here %s is name and %d is age '''
+
+
+color_list = ["Red","Green","White" ,"Black"]
+print("%s %s"% (color_list[0],color_list[-1]))
+'''here %s is color_list[0] , another one color_list[-1])      output will come without comma'''
